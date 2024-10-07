34198c3a03b52ce5e95e138f18996f2ed86cf377
pratik <pratik24r@gmail.com.com>
2024-10-07 10:50:06 +0530

    updted

---
+++ Form1.cs
@@ -0,0 +1,19 @@
+
+using System;
+using System.Windows.Forms;
+
+namespace MyFirstWindowsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, welcome to your first Windows Forms app!");
+        }
+    }
+}
